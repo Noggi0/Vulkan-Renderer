e0479e183d1fccfcf3271b5f4f2f0a28935c3478
Vincent <vincent1.son@epitech.eu>
2021-08-01 20:52:46 +0200

    added GLM GLFW and eventually a cross-platform CMake

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by vince on 01/08/2021.
+//
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+int main() {
+    return 0;
+}
