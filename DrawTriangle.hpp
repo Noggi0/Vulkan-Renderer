3a68e838f939db89534132145b4af1c7ed6c3e42
Vincent Son <vincent1.son@epitech.eu>
2022-02-12 17:30:05 +0100

    Vulkan all set up !

--- DrawTriangle.hpp
+++ DrawTriangle.hpp
@@ -7,9 +7,21 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vector>
+#include <optional>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
 
 class DrawTriangle {
     public:
@@ -20,35 +32,207 @@ class DrawTriangle {
             this->cleanup();
         };
     private:
-        void initVulkan() {
-            this->createInstance();
+        struct QueueFamilyIndices {
+            std::optional<uint32_t> graphicsFamily;
+
+            bool isComplete() {
+                return graphicsFamily.has_value();
+            }
         };
-        void createInstance() {
-            VkApplicationInfo appInfo {};
-            appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-            appInfo.pApplicationName = "Vulkan Renderer";
-            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-            appInfo.pEngineName = "No Engine";
-            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-            appInfo.apiVersion = VK_API_VERSION_1_0;
-        };
+
         void initWindow() {
             glfwInit();
             glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
             glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
             this->window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Renderer", nullptr, nullptr);
         };
+
+        void initVulkan() {
+            this->createInstance();
+            this->pickPhysicalDevice();
+            this->createLogicalDevice();
+        };
+
+        void createInstance() {
+            if (enableValidationLayers && !checkValidationLayerSupport())
+                throw std::runtime_error("validation layers requested, but not available!");
+
+            VkApplicationInfo appInfo {};
+            appInfo.pApplicationName = "Vulkan Renderer";
+            appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.pEngineName = "No Engine";
+            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.apiVersion = VK_API_VERSION_1_0;
+
+            VkInstanceCreateInfo createInfo {};
+            createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+            createInfo.pApplicationInfo = &appInfo;
+
+            uint32_t extensionCount = 0;
+            vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+            std::vector<VkExtensionProperties> extensions(extensionCount);
+            vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+            uint32_t glfwExtensionCount = 0;
+            const char** glfwExtensions;
+
+            glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+            if (!this->areGlfwExtensionsSupported(extensions, glfwExtensions, glfwExtensionCount))
+                throw std::runtime_error("Required extensions are not supported");
+
+            createInfo.enabledExtensionCount = glfwExtensionCount;
+            createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+            if (enableValidationLayers) {
+                createInfo.enabledExtensionCount = static_cast<uint32_t>(validationLayers.size());
+                createInfo.ppEnabledLayerNames = validationLayers.data();
+            } else
+                createInfo.enabledLayerCount = 0;
+
+            if (vkCreateInstance(&createInfo, nullptr, &this->instance) == VK_SUCCESS)
+                std::cout << "Vulkan instance created successfully\n" << std::endl;
+            else
+                throw std::runtime_error("Failed to create Vulkan instance");
+        };
+
+        void pickPhysicalDevice() {
+            uint32_t deviceCount = 0;
+            vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+            if (deviceCount == 0)
+                throw std::runtime_error("failed to find GPUs with Vulkan support!");
+            
+            std::vector<VkPhysicalDevice> devices(deviceCount);
+            vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+            for (const auto& device : devices) {
+                if (isDeviceSuitable(device)) {
+                    physicalDevice = device;
+                    break;
+                }
+            }
+
+            if (physicalDevice == VK_NULL_HANDLE) {
+                throw std::runtime_error("failed to find a suitable GPU!");
+            }
+        };
+
+        bool isDeviceSuitable(VkPhysicalDevice device) {
+            QueueFamilyIndices indices = this->findQueueFamilies(device);
+
+            return indices.isComplete();
+        };
+
+        QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+            QueueFamilyIndices indices;
+            uint32_t queueFamilyCount = 0;
+            vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+            std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+            vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+            int i = 0;
+            for (const auto& queueFamily : queueFamilies) {
+                if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                    indices.graphicsFamily = i;
+                }
+                if (indices.isComplete())
+                    break;
+                i++;
+            }
+            return indices;
+        }
+
+        bool areGlfwExtensionsSupported(std::vector<VkExtensionProperties> extensions, const char** glfwExtensions, uint32_t glfwExtensionsCount) {
+            for (uint32_t i = 0; i < glfwExtensionsCount; i++) {
+                auto glfwExtension = glfwExtensions[i];
+                bool found = false;
+                for (auto extension : extensions) {
+                    if (strcmp(extension.extensionName, glfwExtension) == 0) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                    return false;
+            }
+            return true;
+        };
+
         void mainLoop() {
             while (!glfwWindowShouldClose(this->window)) {
                 glfwPollEvents();
             }
         };
+
+        bool checkValidationLayerSupport() {
+            uint32_t layerCount;
+            vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+            std::vector<VkLayerProperties> availableLayers(layerCount);
+            vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+            for (const char* layerName : validationLayers) {
+                bool layerFound = false;
+
+                for (const auto& layerProperties : availableLayers) {
+                    if (strcmp(layerName, layerProperties.layerName) == 0) {
+                        layerFound = true;
+                        break;
+                    }
+                }
+
+                if (!layerFound) {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        void createLogicalDevice() {
+            QueueFamilyIndices indices = this->findQueueFamilies(this->physicalDevice);
+
+            VkDeviceQueueCreateInfo queueCreateInfo {};
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+            queueCreateInfo.queueCount = 1;
+            float queuePriority = 1.0f;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+
+            VkPhysicalDeviceFeatures deviceFeatures{};
+
+            VkDeviceCreateInfo createInfo {};
+            createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+            createInfo.pQueueCreateInfos = &queueCreateInfo;
+            createInfo.queueCreateInfoCount = 1;
+            createInfo.pEnabledFeatures = &deviceFeatures;
+            createInfo.enabledExtensionCount = 0;
+            if (enableValidationLayers) {
+                createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+                createInfo.ppEnabledLayerNames = validationLayers.data();
+            } else {
+                createInfo.enabledLayerCount = 0;
+            }
+
+            if (vkCreateDevice(this->physicalDevice, &createInfo, nullptr, &this->device) != VK_SUCCESS)
+                throw std::runtime_error("failed to create logical device!");
+
+            vkGetDeviceQueue(this->device, indices.graphicsFamily.value(), 0, &this->graphicsQueue);
+        }
+
         void cleanup() {
+            vkDestroyDevice(this->device, nullptr);
+            vkDestroyInstance(this->instance, nullptr);
             glfwDestroyWindow(this->window);
             glfwTerminate();
         };
+
         GLFWwindow *window;
         VkInstance instance;
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+        VkDevice device = VK_NULL_HANDLE;
+        VkQueue graphicsQueue;
 };
 
 #endif //RENDERER_DRAWTRIANGLE_HPP
---
+++ Librairies/GLFW/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/cocoa_init.m" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_init.m.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_init.m.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/cocoa_joystick.m" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/cocoa_monitor.m" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/cocoa_time.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_time.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_time.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/cocoa_window.m" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_window.m.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/cocoa_window.m.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/context.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/context.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/context.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/egl_context.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/egl_context.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/egl_context.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/init.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/init.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/init.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/input.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/input.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/input.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/monitor.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/monitor.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/monitor.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/nsgl_context.m" "Librairies/GLFW/src/CMakeFiles/glfw.dir/nsgl_context.m.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/nsgl_context.m.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/osmesa_context.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/osmesa_context.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/osmesa_context.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/posix_thread.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/posix_thread.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/posix_thread.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/vulkan.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/vulkan.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/vulkan.c.o.d"
+  "/Users/vincentson/code/Side Projects/Vulkan-Renderer/Librairies/GLFW/src/window.c" "Librairies/GLFW/src/CMakeFiles/glfw.dir/window.c.o" "gcc" "Librairies/GLFW/src/CMakeFiles/glfw.dir/window.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
